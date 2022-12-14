9d8f81bd31482eee3f99a0dedc20278f7d70fae1
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-12-14 12:47:47 +0530

    Update avl.cpp

--- avl.cpp
+++ avl.cpp
@@ -1,287 +1,259 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-  
- 
-class Node  
-{  
-    public: 
-    int key;  
-    Node *left;  
-    Node *right;  
-    int height;  
-};  
-  
-  
-int max(int a, int b);  
-  
+#include <iostream>
+using namespace std;
 
-int height(Node *N)  
-{  
-    if (N == NULL)  
-        return 0;  
-    return N->height;  
-}  
-  
+class Node {
+    int *keys;
+    int t;
+    Node **C;
+    int n;
+    bool leaf;
 
-int max(int a, int b)  
-{  
-    return (a > b)? a : b;  
-}  
-  
+   public:
+    Node(bool _leaf);
+    void traverse();
+    Node *search(int k);
+    int findKey(int k);
+    void insertNonFull(int k);
+    void splitChild(int i, Node *y);
+    int getPred(int idx);
+    int getSucc(int idx);
+    void fill(int idx);
+    void borrowFromPrev(int idx);
+    void borrowFromNext(int idx);
+    void merge(int idx);
+    friend class Tree;
+};
 
-Node* newNode(int key)  
-{  
-    Node* node = new Node(); 
-    node->key = key;  
-    node->left = NULL;  
-    node->right = NULL;  
-    node->height = 1; 
-    return(node);  
-}  
-  
- 
-Node *rightRotate(Node *y)  
-{  
-    Node *x = y->left;  
-    Node *T2 = x->right;  
-  
-     
-    x->right = y;  
-    y->left = T2;  
-  
-    
-    y->height = max(height(y->left),  
-                    height(y->right)) + 1;  
-    x->height = max(height(x->left),  
-                    height(x->right)) + 1;  
-  
-   
-    return x;  
-}  
-  
+class Tree {
+    Node *root;
+    int t;
 
-Node *leftRotate(Node *x)  
-{  
-    Node *y = x->right;  
-    Node *T2 = y->left;  
-  
-  
-    y->left = x;  
-    x->right = T2;  
-  
-      
-    x->height = max(height(x->left),  
-                    height(x->right)) + 1;  
-    y->height = max(height(y->left),  
-                    height(y->right)) + 1;  
-  
-  
-    return y;  
-}  
-  
- 
-int getBalance(Node *N)  
-{  
-    if (N == NULL)  
-        return 0;  
-    return height(N->left) -  
-           height(N->right);  
-}  
-  
-Node* insert(Node* node, int key)  
-{  
-    
-    if (node == NULL)  
-        return(newNode(key));  
-  
-    if (key < node->key)  
-        node->left = insert(node->left, key);  
-    else if (key > node->key)  
-        node->right = insert(node->right, key);  
-    else  
-        return node;  
-  
-    
-    node->height = 1 + max(height(node->left),  
-                           height(node->right));  
-  
-    
-    int balance = getBalance(node);  
-  
-     
-  
-     
-    if (balance > 1 && key < node->left->key)  
-        return rightRotate(node);  
-  
-    
-    if (balance < -1 && key > node->right->key)  
-        return leftRotate(node);  
-  
-     
-    if (balance > 1 && key > node->left->key)  
-    {  
-        node->left = leftRotate(node->left);  
-        return rightRotate(node);  
-    }  
-  
-    
-    if (balance < -1 && key < node->right->key)  
-    {  
-        node->right = rightRotate(node->right);  
-        return leftRotate(node);  
-    }  
-  
-  
-    return node;  
-}  
-  
+   public:
+    Tree() {
+        root = NULL;
+        t = 2;
+    }
 
-Node * minValueNode(Node* node)  
-{  
-    Node* current = node;  
-  
-   
-    while (current->left != NULL)  
-        current = current->left;  
-  
-    return current;  
-}  
-  
+    void traverse() {
+        if (root != NULL)
+            root->traverse();
+    }
 
-Node* deleteNode(Node* root, int key)  
-{  
-      
-    
-    if (root == NULL)  
-        return root;  
-  
-      
-    if ( key < root->key )  
-        root->left = deleteNode(root->left, key);  
-  
-     
-    else if( key > root->key )  
-        root->right = deleteNode(root->right, key);  
-  
-     
-    else
-    {  
-         
-        if( (root->left == NULL) || 
-            (root->right == NULL) )  
-        {  
-            Node *temp = root->left ?  
-                         root->left :  
-                         root->right;  
-  
-             
-            if (temp == NULL)  
-            {  
-                temp = root;  
-                root = NULL;  
-            }  
-            else   
-            *root = *temp;   
-                            
-            free(temp);  
-        }  
+    Node *search(int k) {
+        return (root == NULL) ? NULL : root->search(k);
+    }
+
+    void insert(int k);
+};
+
+Node::Node(bool leaf1) {
+    t = 2;
+    leaf = leaf1;
+    keys = new int[2 * t - 1];
+    C = new Node *[2 * t];
+    n = 0;
+}
+
+int Node::findKey(int k) {
+    int idx = 0;
+    while (idx < n && keys[idx] < k)
+        ++idx;
+    return idx;
+}
+
+int Node::getPred(int idx) {
+    Node *cur = C[idx];
+    while (!cur->leaf)
+        cur = cur->C[cur->n];
+    return cur->keys[cur->n - 1];
+}
+
+int Node::getSucc(int idx) {
+    Node *cur = C[idx + 1];
+    while (!cur->leaf)
+        cur = cur->C[0];
+    return cur->keys[0];
+}
+
+void Node::fill(int idx) {
+    if (idx != 0 && C[idx - 1]->n >= t)
+        borrowFromPrev(idx);
+    else if (idx != n && C[idx + 1]->n >= t)
+        borrowFromNext(idx);
+    else {
+        if (idx != n)
+            merge(idx);
         else
-        {  
-             
-            Node* temp = minValueNode(root->right);  
-  
-             
-            root->key = temp->key;  
-  
-             
-            root->right = deleteNode(root->right,  
-                                     temp->key);  
-        }  
-    }  
-  
-  
-    if (root == NULL)  
-    return root;  
-  
-     
-    root->height = 1 + max(height(root->left),  
-                           height(root->right));  
-  
-     
-    int balance = getBalance(root);  
-  
-     
-    if (balance > 1 &&  
-        getBalance(root->left) >= 0)  
-        return rightRotate(root);  
-  
-     
-    if (balance > 1 &&  
-        getBalance(root->left) < 0)  
-    {  
-        root->left = leftRotate(root->left);  
-        return rightRotate(root);  
-    }  
-  
-      
-    if (balance < -1 &&  
-        getBalance(root->right) <= 0)  
-        return leftRotate(root);  
-  
-   
-    if (balance < -1 &&  
-        getBalance(root->right) > 0)  
-    {  
-        root->right = rightRotate(root->right);  
-        return leftRotate(root);  
-    }  
-  
-    return root;  
-}  
-  
- 
-void preOrder(Node *root)  
-{  
-    if(root != NULL)  
-    {  
-        cout << root->key << " ";  
-        preOrder(root->left);  
-        preOrder(root->right);  
-    }  
-}  
-  
+            merge(idx - 1);
+    }
+    return;
+}
 
-int main()  
-{  
-Node *root = NULL;  
-  
-   int n,in,del;
-   cout<<"Enter no of nodes in the tree"<<endl;
-   cin>>n;
-   cout<<"Enter the node"<<endl;
-   for(int i=0;i<n;i++)
-   {
-    
-    cin>>in;
-    root = insert(root, in);  
-  }
-    
-  
-    cout << "Preorder traversal of the "
-            "constructed AVL tree is \n";  
-    preOrder(root);  
-   cout<<endl;
-    cout<<"Enter the element to be deleted"<<endl;
-    cin>>del;
-    root = deleteNode(root, del);  
-  
-   
-    cout << "\nPreorder traversal after"
-         << " deletion  \n";  
-    preOrder(root);  
-  
-    return 0;  
-}  
-  
-  
+void Node::borrowFromPrev(int idx) {
+    Node *child = C[idx];
+    Node *sibling = C[idx - 1];
+    for (int i = child->n - 1; i >= 0; --i)
+        child->keys[i + 1] = child->keys[i];
+    if (!child->leaf) {
+        for (int i = child->n; i >= 0; --i)
+            child->C[i + 1] = child->C[i];
+    }
+    child->keys[0] = keys[idx - 1];
+    if (!child->leaf)
+        child->C[0] = sibling->C[sibling->n];
+    keys[idx - 1] = sibling->keys[sibling->n - 1];
+    child->n += 1;
+    sibling->n -= 1;
+    return;
+}
+
+void Node::borrowFromNext(int idx) {
+    Node *child = C[idx];
+    Node *sibling = C[idx + 1];
+    child->keys[(child->n)] = keys[idx];
+    if (!(child->leaf))
+        child->C[(child->n) + 1] = sibling->C[0];
+    keys[idx] = sibling->keys[0];
+
+    for (int i = 1; i < sibling->n; ++i)
+        sibling->keys[i - 1] = sibling->keys[i];
+    if (!sibling->leaf) {
+        for (int i = 1; i <= sibling->n; ++i)
+            sibling->C[i - 1] = sibling->C[i];
+    }
+    child->n += 1;
+    sibling->n -= 1;
+    return;
+}
+
+void Node::merge(int idx) {
+    Node *child = C[idx];
+    Node *sibling = C[idx + 1];
+    child->keys[t - 1] = keys[idx];
+    for (int i = 0; i < sibling->n; ++i)
+        child->keys[i + t] = sibling->keys[i];
+    if (!child->leaf) {
+        for (int i = 0; i <= sibling->n; ++i)
+            child->C[i + t] = sibling->C[i];
+    }
+    for (int i = idx + 1; i < n; ++i)
+        keys[i - 1] = keys[i];
+    for (int i = idx + 2; i <= n; ++i)
+        C[i - 1] = C[i];
+    child->n += sibling->n + 1;
+    n--;
+    delete (sibling);
+    return;
+}
+
+void Tree::insert(int k) {
+    if (root == NULL) {
+        root = new Node(true);
+        root->keys[0] = k;
+        root->n = 1;
+    } else {
+        if (root->n == 2 * t - 1) {
+            Node *s = new Node(false);
+            s->C[0] = root;
+            s->splitChild(0, root);
+            int i = 0;
+            if (s->keys[0] < k)
+                i++;
+            s->C[i]->insertNonFull(k);
+            root = s;
+        } else
+            root->insertNonFull(k);
+    }
+}
+
+void Node::insertNonFull(int k) {
+    int i = n - 1;
+    if (leaf == true) {
+        while (i >= 0 && keys[i] > k) {
+            keys[i + 1] = keys[i];
+            i--;
+        }
+        keys[i + 1] = k;
+        n = n + 1;
+    } else {
+        while (i >= 0 && keys[i] > k)
+            i--;
+        if (C[i + 1]->n == 2 * t - 1) {
+            splitChild(i + 1, C[i + 1]);
+            if (keys[i + 1] < k)
+                i++;
+        }
+        C[i + 1]->insertNonFull(k);
+    }
+}
+
+void Node::splitChild(int i, Node *y) {
+    Node *z = new Node(y->leaf);
+    z->n = t - 1;
+    for (int j = 0; j < t - 1; j++)
+        z->keys[j] = y->keys[j + t];
+    if (y->leaf == false) {
+        for (int j = 0; j < t; j++)
+            z->C[j] = y->C[j + t];
+    }
+    y->n = t - 1;
+    for (int j = n; j >= i + 1; j--)
+        C[j + 1] = C[j];
+
+    C[i + 1] = z;
+    for (int j = n - 1; j >= i; j--)
+        keys[j + 1] = keys[j];
+
+    keys[i] = y->keys[t - 1];
+    n = n + 1;
+}
+
+void Node::traverse() {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (leaf == false)
+            C[i]->traverse();
+        cout << " " << keys[i];
+    }
+    if (leaf == false)
+        C[i]->traverse();
+}
+
+Node *Node::search(int k) {
+    int i = 0;
+    while (i < n && k > keys[i])
+        i++;
+    if (keys[i] == k)
+        return this;
+1
+    if (leaf == true)
+        return NULL;
+    return C[i]->search(k);
+}
+
+int main() {
+    Tree t;
+    cout << "1. Insert\n2. Display Tree\n3. Exit" << endl;
+    int choice, node;
+    do {
+        cout << "Enter choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Enter node: ";
+                cin >> node;
+                t.insert(node);
+                break;
+            case 2:
+                cout << "Tree is \n";
+                t.traverse();
+                cout << endl;
+                break;
+            case 3:
+                return 0;
+            default:
+                cout << "Enter valid choice!" << endl;
+        }
+    } while (choice != 4);
+    return 0;
+}
